bdc9385750e921f3e871a772db02aa0950490340
Rianifitriim <rianifitri53@gmail.com>
2021-09-22 17:28:11 +0700

    membuat check-string-blank-or-not

---
+++ main.js
@@ -0,0 +1,12 @@
+function isBlank (x){
+    if(isBlank==='')
+    {
+      console.log('true');
+      console.log(x);
+    }
+    else {
+      console.log('false') 
+    } 
+  }
+  
+  test('abc');
